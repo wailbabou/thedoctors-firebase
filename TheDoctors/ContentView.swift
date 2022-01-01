bcf2e03b31955504da110303163fef23419ff9d1
Ouail Bellal <ouailbellal@gmail.com>
2022-01-01 10:15:25 -0500

    Initial Commit

---
+++ TheDoctors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheDoctors
+//
+//  Created by mac on 1/1/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheDoctors/TheDoctorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheDoctorsApp.swift
+//  TheDoctors
+//
+//  Created by mac on 1/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheDoctorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
